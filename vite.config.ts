e1ea81b6b38682b938930fa3325e7d35e69c6d7c
Lucas Álek <lucas.maistro@gmail.com>
2023-06-22 19:52:11 -0300

    fix: build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import webExtension from "vite-plugin-web-extension"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), webExtension()],
 })
