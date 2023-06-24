79ab84b938c896f2265490316ec3a1aff267c9de
Lucas Álek <lucas.maistro@gmail.com>
2023-06-24 12:17:47 -0300

    add: beta ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,26 @@
 import { useLayoutEffect, useState } from "preact/hooks"
+import { styled } from "styled-components"
+import { Note } from "./types"
+import { DB_NAME, DB_VERSION, STORE_NOTES } from "./config"
+import {
+  theme,
+  SaguGlobalStyles,
+  SaguProvider,
+  Box,
+  Heading,
+  Button,
+  Divider,
+  wine,
+  NavLink,
+  TextContent,
+  Ribbon,
+} from "sagu-ui"
 
-import "./app.css"
+Object.assign(theme.colors, wine)
+
+const StyledMain = styled.main`
+  width: 520px;
+`
 
 export function App() {
   const [notes, setNotes] = useState([])
@@ -10,19 +30,7 @@ export function App() {
   }, [])
 
   const requestNotes = async () => {
-    // const [tab] = await chrome.tabs.query({
-    //   active: true,
-    //   lastFocusedWindow: true,
-    // })
-    // const response = await chrome.tabs.sendMessage(tab.id || 0, {
-    //   type: "GET_DATA",
-    // })
-
-    const DB_NAME = "side-notes"
-    const DB_VERSION = 1
-    const STORE_NOTES = "notes"
-
-    let db
+    let db: any
 
     const dbConnection = indexedDB.open(DB_NAME, DB_VERSION)
     dbConnection.onsuccess = (event: any) => {
@@ -32,47 +40,124 @@ export function App() {
       const getAllRequest = store.getAll()
 
       getAllRequest.onsuccess = (e: any) => {
-        console.log(e)
         setNotes(e.target.result)
+        db.close()
       }
     }
   }
 
   const requestDelete = async (noteId: any) => {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true,
-    })
-    const response = await chrome.tabs.sendMessage(tab.id || 0, {
-      type: "DeleteData",
-      payload: noteId,
-    })
+    let db: any
 
-    console.log(response)
+    const dbConnection = indexedDB.open(DB_NAME, DB_VERSION)
+
+    dbConnection.onsuccess = (event: any) => {
+      db = event.target?.result
+      const transaction = db.transaction([STORE_NOTES], "readwrite")
+      const store = transaction.objectStore(STORE_NOTES)
+      const deleteRequest = store.delete(noteId)
+
+      deleteRequest.onsuccess = (e: any) => {
+        setNotes(e)
+        db.close()
+      }
+
+      deleteRequest.onerror = () => {
+        db.close()
+      }
+    }
   }
 
-  const handleNavigation = (url: URL) => {
+  const handleNavigation = (event: Event, url: URL) => {
+    event.preventDefault()
     window.open(url, "_blank", "noreferrer")
   }
 
   return (
-    <>
-      <h1>Side Notes</h1>
-      <ol>
-        {notes.map((note: any) => (
-          <li className={"listItem"} onClick={() => handleNavigation(note.url)}>
-            <p> {note.content}</p>
-            <p>
-              <i>
-                <small>{note.date}</small>
-              </i>
-            </p>
-            <small className={"listItem__actions"}>
-              <i onClick={() => requestDelete(note.id)}>apagar</i>
-            </small>
-          </li>
-        ))}
-      </ol>
-    </>
+    <SaguProvider theme={theme}>
+      <SaguGlobalStyles />
+      <StyledMain>
+        <Box flex="column" gap="large" border shadow padding="medium">
+          <Ribbon>Beta</Ribbon>
+          <Heading lineLeft lineColor="secondary" size="huge">
+            Side Notes
+          </Heading>
+          <Divider />
+          <Box
+            shadow={false}
+            flex="row"
+            gap="xsmall"
+            fullWidth={true}
+            justify="end"
+          >
+            <Button
+              size="xsmall"
+              outline={false}
+              padding="none"
+              onClick={() => setNotes([])}
+            >
+              Delete all
+            </Button>
+            <Button
+              size="xsmall"
+              outline={false}
+              padding="none"
+              onClick={() => setNotes(notes)}
+            >
+              Restore
+            </Button>
+          </Box>
+          <ul>
+            {notes.length ? (
+              notes.map((note: Note) => (
+                <li>
+                  <Box
+                    flex="row"
+                    gap="xxsmall"
+                    padding="xxsmall"
+                    alignment="center"
+                    justify="space-between"
+                    fullWidth
+                  >
+                    <NavLink
+                      size="small"
+                      onClick={(e: Event) => handleNavigation(e, note.url)}
+                      padding="none"
+                    >
+                      {note.content}
+                    </NavLink>
+                    <Button
+                      variant="filled"
+                      padding="mini"
+                      size="xsmall"
+                      onClick={() => requestDelete(note.id)}
+                    >
+                      Delete
+                    </Button>
+                  </Box>
+                </li>
+              ))
+            ) : (
+              <TextContent value="There is no note..." />
+            )}
+          </ul>
+          <Box
+            padding="xxsmall"
+            fullWidth
+            style={{
+              height: "min-content",
+              textAlign: "right",
+            }}
+          >
+            <TextContent
+              tag="i"
+              color="black"
+              value="Powered by Sagu-UI"
+              size="xsmall"
+            />
+          </Box>
+        </Box>
+      </StyledMain>
+    </SaguProvider>
   )
 }
