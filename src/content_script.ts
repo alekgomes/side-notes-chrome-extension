09a3c191b9ea6eb64c19083b42a0275f087bdecb
Lucas Álek <lucas.maistro@gmail.com>
2023-07-12 17:34:17 -0300

    wip: make style persist over reloads

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,7 +1,19 @@
+import { GET_NOTE_FROM_USER } from "./types"
+
 window.onload = () => {
   chrome.runtime.onMessage.addListener(({ type }, _sender, sendResponse) => {
+    console.log("clicado")
     switch (type) {
-      case "GET_NOTE_FROM_USER": {
+      case GET_NOTE_FROM_USER: {
+        const selection = window.getSelection()
+        if (selection && selection.rangeCount > 0) {
+          const range = selection.getRangeAt(0)
+          var span = document.createElement("span")
+          span.style.background = "rgba(255,0,0)"
+          span.dataset.sidenoteId = "algumId"
+          range.surroundContents(span)
+        }
+
         return sendResponse({
           content: window.getSelection()?.toString(),
           date: Date.now(),
